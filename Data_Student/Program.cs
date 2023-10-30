40c74ab2898867beb99d7bf196fd208f0708aad6
malakysh <149428349+malakysh@users.noreply.github.com>
2023-10-30 13:16:49 -0700

    Add files via upload

---
+++ Data_Student/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Data_Student
+{
+    class Program:Student
+    {
+        //public static int x = 0; //   متغير لحفظ عدد الطلاب
+        public static int opr;  //  متغير لحفظ رقم العملية
+        static void Main(string[] args)
+        {
+            Console.Title = "Data_Of_Student"; //   تغيير عنوان النافذة
+            //       رسالة الترحيب
+            //---------------------------------------------------------------------------
+            try
+            {
+                Console.WriteLine("\n\n\n\n\n\n\n\n\t\t\t\t\t#################################");
+                Console.WriteLine("\n\n\n\t\t\t\t\t\t.....  Welcome Nanny Ali  .....");
+                Console.WriteLine("\n\n\n\t\t\t\t\t#################################\n");
+                string fllowing = Console.ReadLine();
+                Console.Clear();
+            }
+            catch
+            {
+                MessageBox.Show(" The data is incorrect");
+            }
+            //---------------------------------------------------------------------------
+
+            Student s = new Student(); //  اشتقاق كائن من كلاس الطالب
+
+
+        //---------------------------------------------------------------------------
+
+        //          إختيار العمليات 
+        //---------------------------------------------------------------------------
+
+
+        a1:
+            Console.Clear();
+                Console.WriteLine("\n\n\n\t\tChoose one of the following operations :\n");
+                Console.WriteLine("\t\t\t1- Add");
+                Console.WriteLine("\t\t\t2- View all");
+                Console.WriteLine("\t\t\t3- Delete");
+                Console.WriteLine("\t\t\t4- Amendment");
+                Console.WriteLine("\t\t\t5- Search");
+                Console.WriteLine("\t\t\t6- Exit\n\n\n\t\t\t\t\t");
+                opr = Convert.ToInt16(Console.ReadLine()); //  متغير لحفظ رقم العملية
+                Console.Clear();
+            
+
+                //---------------------------------------------------------------------------
+
+            // إستدعاء الدوال من كلاس الطالب بحسب رقم العملية المختارة من قبل المستخدم 
+
+
+            if (opr == 1) // دالة إضافة البيانات
+            { // start if (opr==1)
+
+                try {
+                    s.add(); // استدعاء دالة الإضافة
+                    MessageBox.Show("تم الإضافة بنجاح");
+                    Console.Clear();
+                }
+                catch
+                {
+                    MessageBox.Show("القيمة غير صحيحة");                }
+            }// end if (opr==1)
+
+            else if (opr == 2) // start if (opr==2)
+            {
+
+                Console.Clear();
+                s.View(); // إستدعاء دالة العرض
+                string b = Console.ReadLine(); // لبقاء الصفحة الحالية بدون مسح
+                Console.Clear();
+
+            }// end if (opr==2)
+            else if (opr == 3)
+            {// start if (opr==3)
+                try {
+                    Console.Clear();
+                    Console.Write("\n\n\t Enter The Student_ID : ");
+                    int d = Convert.ToInt16(Console.ReadLine());
+                    s.delete(d); // إستدعاء دالة الحذف
+                }
+                catch
+                {
+                    MessageBox.Show("هذا الرقم غير موجود");
+                }
+            } // end if (opr==3)
+            else if (opr == 4)
+            {
+                try {
+                    Console.Clear();
+                    Console.Write("\n\n\t Enter The Student_ID : ");
+                    int a = Convert.ToInt16(Console.ReadLine());
+                    s.Amendment(a); // استدعاء دالة التعديل
+                }
+                catch
+                {
+                    MessageBox.Show("هذا الرقم غير موجود");
+                }
+            }
+            else if (opr == 5)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.Write("\n\n\t Enter The Student_ID : ");
+                    int se = Convert.ToInt16(Console.ReadLine());
+                    s.Search(se); // استدعاء دالة البحث
+                    string b = Console.ReadLine(); // لبقاء الصفحة الحالية بدون مسح
+                }
+                catch
+                {
+                    MessageBox.Show("هذا الرقم غير موجود");
+                }
+            }
+            else 
+            {
+                Application.Exit();Environment.Exit(0);
+            }
+            goto a1; // العودة الى القائمة الرئيسية
+        
+            //---------------------------------------------------------------------------
+            
+            Console.ReadKey(); 
+        }
+        //------------------------------------------------------------------------------------------------------------------------
+
+       
+
+    }
+
+}
---
+++ Data_Student/Student.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Data_Student
+{
+     class Student:Program
+    {
+         public static int x=12; //   متغير لحفظ عدد الطلاب
+
+
+        //------------------------------------------------------------------------------------------------------------------------
+
+        public struct stu // سجل يحتوي على البيانات االرئيسية للطالب
+        {
+            public static int[] id = new int[x];
+            public static string[] name = new string[x];
+            public static int[] age = new int[x];
+            public static string[] address = new string[x];
+            public static int[] phone = new int[x];
+            public static string[] stage = new string[x];
+            public static double[] d1 = new double[x];
+            public static double[] d2 = new double[x];
+            public static double[] d3 = new double[x];
+            public static double[] d4 = new double[x];
+            public static double[] d5 = new double[x];
+            public static double[] d6 = new double[x];
+        };
+        //------------------------------------------------------------------------------------------------------------------------
+        public int t=1;
+            public  void add() //  دالة لإضافة بيانات طالب
+            {
+            try
+            {
+                 for (int i = t-1; i < t; i++)
+                {
+                Console.Clear();
+                //  البيانات الرئيسية للطالب
+                Console.Write("\tPlease Enter Student_ID : ");
+                    stu.id[i] = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Name : ");
+                    stu.name[i] = Console.ReadLine();
+                    Console.Write("\tPlease Enter Student_Age : ");
+                    stu.age[i] = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Address : ");
+                    stu.address[i] = Console.ReadLine();
+                    Console.Write("\tPlease Enter Student_Phone : ");
+                    stu.phone[i] = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Stage : ");
+                    stu.stage[i] = Console.ReadLine();
+
+                //  درجات الطالب
+                Console.WriteLine("\n-------------------------------------");
+                    Console.Write("\tPlease Enter Student_Degree1 : ");
+                    stu.d1[i] = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Degree2 : ");
+                    stu.d2[i] = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Degree3 : ");
+                    stu.d3[i] = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Degree4 : ");
+                    stu.d4[i] = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Degree5 : ");
+                    stu.d5[i] = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Degree6 : ");
+                    stu.d6[i] = Convert.ToDouble(Console.ReadLine());
+                }
+            t++;
+            }
+            catch (e)
+            {
+                
+                e;
+            }
+               
+        }
+
+
+            //------------------------------------------------------------------------------------------------------------------------
+
+            public  void View()
+            {
+            try
+            {
+                  for (int i = 0; i < t; i++)
+                {
+                if (stu.id[i] != 0)
+                {
+
+                
+                    Console.WriteLine("####################################################################################################################");
+                    Console.Write("\n\n\n\t Student_ID : " + stu.id[i]);
+                    Console.Write("\n\t Student_Name : " + stu.name[i]);
+                    Console.Write("\n\t Student_Age : " + stu.age[i]);
+                    Console.Write("\n\t Student_Address : " + stu.address[i]);
+                    Console.Write("\n\t Student_Phone : " + stu.phone[i]);
+                    Console.Write("\n\t Student_Stage : " + stu.stage[i]);
+                    Console.WriteLine("\n------------------------------------------------------------------------------------------------");
+                    Console.Write("\n\t Degree1\t Degree2\t Degree3\t Degree4\t Degree5\t Degree6");
+                    Console.Write("\n\t   " + stu.d1[i] + "\t\t  " + stu.d2[i] + "\t\t  " + stu.d3[i] + "\t\t  " + stu.d4[i] + "\t\t  " + stu.d5[i] + "\t\t  " + stu.d6[i]);
+                    Console.WriteLine("\n------------------------------------------------------------------------------------------------\n");
+                    double sum = (stu.d1[i] + stu.d2[i] + stu.d3[i] + stu.d4[i] + stu.d5[i] + stu.d6[i]);
+                     double avg = sum / 6;
+                    Console.WriteLine("\n\t Sum : " +sum+"\t AVG : "+avg );
+                    Console.WriteLine("\n------------------------------------------------------------------------------------------------\n\n");
+                }
+
+            }
+            }
+            catch (e)
+            {
+                
+                EncoderFallbackException;
+            }
+              
+            }
+        public void delete(int c)
+        {
+            try
+            {
+                  for(int i=0;i<=t; i++)
+            {
+                if (stu.id[i] == c)
+                {
+                    stu.id[i] = 0;
+                    MessageBox.Show("تم الحذف بنجاح");
+                    Console.Clear();
+                }
+            }}
+            catch (System.Exception)
+            {
+                
+                System.Exception;
+            }
+          
+              
+            }
+        }
+        public void Amendment(int a)
+        {
+            try
+            {
+                  for (int i = 0; i <= t; i++)
+            {
+                if (stu.id[i] == a)
+                {
+                    Console.Clear();
+                    Console.Write("\tPlease Enter Student_Name : ");
+                    stu.name[i] = Console.ReadLine();
+                    Console.Write("\tPlease Enter Student_Age : ");
+                    stu.age[i] = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Address : ");
+                    stu.address[i] = Console.ReadLine();
+                    Console.Write("\tPlease Enter Student_Phone : ");
+                    stu.phone[i] = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Stage : ");
+                    stu.stage[i] = Console.ReadLine();
+
+                    //  درجات الطالب
+                    Console.WriteLine("\n-------------------------------------");
+                    Console.Write("\tPlease Enter Student_Degree1 : ");
+                    stu.d1[i] = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Degree2 : ");
+                    stu.d2[i] = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Degree3 : ");
+                    stu.d3[i] = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Degree4 : ");
+                    stu.d4[i] = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Degree5 : ");
+                    stu.d5[i] = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\tPlease Enter Student_Degree6 : ");
+                    stu.d6[i] = Convert.ToDouble(Console.ReadLine());
+                    MessageBox.Show("تم التعديل بنجاح");
+                    Console.Clear();
+                }
+            
+            
+            }
+            }
+            catch (System.Exception)
+            {
+                
+                System.Exception;
+            }
+          
+        }
+        public void Search(int s)
+        {
+            try
+            {
+                Console.Clear();
+            for (int i=0;i<= t; i++)
+            {
+                
+                if (stu.id[i] == s)
+                {
+                    Console.WriteLine("####################################################################################################################");
+                    Console.Write("\n\n\n\t Student_ID : " + stu.id[i]);
+                    Console.Write("\n\t Student_Name : " + stu.name[i]);
+                    Console.Write("\n\t Student_Age : " + stu.age[i]);
+                    Console.Write("\n\t Student_Address : " + stu.address[i]);
+                    Console.Write("\n\t Student_Phone : " + stu.phone[i]);
+                    Console.Write("\n\t Student_Stage : " + stu.stage[i]);
+                    Console.WriteLine("\n------------------------------------------------------------------------------------------------");
+                    Console.Write("\n\t Degree1\t Degree2\t Degree3\t Degree4\t Degree5\t Degree6");
+                    Console.Write("\n\t   " + stu.d1[i] + "\t\t  " + stu.d2[i] + "\t\t  " + stu.d3[i] + "\t\t  " + stu.d4[i] + "\t\t  " + stu.d5[i] + "\t\t  " + stu.d6[i]);
+                    Console.WriteLine("\n------------------------------------------------------------------------------------------------\n");
+                    double sum = (stu.d1[i] + stu.d2[i] + stu.d3[i] + stu.d4[i] + stu.d5[i] + stu.d6[i]);
+                    double avg = sum / 6;
+                    Console.WriteLine("\n\t Sum : " + sum + "\t AVG : " + avg);
+                    Console.WriteLine("\n------------------------------------------------------------------------------------------------\n\n");
+                }
+                
+                
+            }
+            }
+            catch (System.Exception)
+            {
+                
+                System.Exception;
+            }
+            
+            
+        }
+        }
+
+    
+
